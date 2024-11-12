8176fb1e098cad46462bb367d0a614783c3165e1
Sissi Feng <38707787+SissiFeng@users.noreply.github.com>
2024-11-12 14:20:15 -0500

    Update color_mixer.py

--- color_mixer.py
+++ color_mixer.py
@@ -1,4 +1,31 @@
+import numpy as np
+from src.mock_light_mixer import MockLightMixer
+
 class ColorMixer:
+    def __init__(self):
+        """Initialize your variables here"""
+        # TODO: Initialize the MockLightMixer and get the light matrix
+        pass
+
+    def match_color(self, target_color):
+        """
+        Match the target RGB color by finding appropriate light intensities.
+        
+        Args:
+            target_color: List of 3 integers (0-255) representing RGB values
+                         Example: [255, 128, 0] for orange
+        
+        Returns:
+            List of 3 float values (0-1) representing the intensity of each light
+            Example: [0.8, 0.4, 0.0]
+        """
+        # TODO: Implement your color matching algorithm here
+        # 1. Convert target_color from 0-255 range to 0-1 range
+        # 2. Use the light_matrix to find appropriate intensities
+        # 3. Make sure intensities are between 0 and 1
+        # 4. Return the intensities as a list
+        pass
+
     @staticmethod
     def mix_colors(color1, color2):
         """
@@ -6,6 +33,8 @@ class ColorMixer:
         Colors should be in the format (R, G, B) where R, G, and B are integers from 0 to 255.
         """
         # TODO: Implement color mixing logic
+        # 1. Mix the two colors (hint: try averaging their values)
+        # 2. Make sure the result is in valid range (0-255)
         pass
 
     @staticmethod
@@ -16,6 +45,9 @@ class ColorMixer:
         Amount should be a float between 0 and 1.
         """
         # TODO: Implement color lightening logic
+        # 1. Check if amount is between 0 and 1
+        # 2. Move the color towards white by the given amount
+        # 3. Make sure the result is in valid range (0-255)
         pass
 
     @staticmethod
@@ -26,6 +58,7 @@ class ColorMixer:
         Amount should be a float between 0 and 1.
         """
         # TODO: Implement color darkening logic
+        # 1. Check if amount is between 0 and 1
+        # 2. Move the color towards black by the given amount
+        # 3. Make sure the result is in valid range (0-255)
         pass
-
-# You can add more methods or helper functions as needed
