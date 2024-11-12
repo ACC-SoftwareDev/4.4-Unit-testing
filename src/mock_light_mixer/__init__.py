9d1e82b11af006b1373bed430b9a9f051b5145e2
Sissi Feng <38707787+SissiFeng@users.noreply.github.com>
2024-11-12 14:22:11 -0500

    Update __init__.py

--- src/mock_light_mixer/__init__.py
+++ src/mock_light_mixer/__init__.py
@@ -1,2 +1,4 @@
 """Mock function that  gets installed by requirements.txt"""
-from color_matching._color_matching import grid_color_search, random_color_search
+from ._mock_light_mixer import MockLightMixer
+
+__all__ = ['MockLightMixer']
